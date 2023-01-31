6e28ac4d705921bd8ca28ccdc2af20b44fff2c1a
nanachichi <116679354+nanachichi@users.noreply.github.com>
2023-01-31 17:34:08 +0200

    Add a function to play one round
    It takes two parameters (playerSelection and computerSelection) and then
    returns a string that declares a winner.

--- script.js
+++ script.js
@@ -11,8 +11,38 @@
 // PROGRAM getComputerChoice()
 //   IF first item in array then return 'Rock'
 
-rps = ['rock', 'paper', 'skissors'];
+const rps = ['rock', 'paper', 'skissors'];
 function getComputerChoice(choices) {
   let choice = Math.floor(Math.random() * 3);
   return choices[choice];
+}
+
+// PROGRAM playRound(playerSelection, computerSelection)
+//   ASK user to choose 'rock', 'paper' or 'skissors'
+//   STORE the choice in a variable
+//   IF user choice is 'rock' and computer choice is 'paper'
+//     user lose
+
+function playRound(playerSelection, computerSelection) {
+  //let computerSelection = getComputerChoice(rps);
+  //let playerSelection = prompt("Select your weapon!\nType rock, paper or skissors: ");
+  if (playerSelection === 'rock' && computerSelection === 'paper') {
+    return "Paper beats rock.\nPlayer lost.";
+  } else if (playerSelection === 'rock' && computerSelection === 'skissors') {
+    return "Rock beats skissors.\nComputer lost.";
+  } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+    return "Paper beats rock.\nComputer lost.";
+  } else if (playerSelection === 'paper' && computerSelection === 'skissors') {
+    return "Skissors beat paper.\nPlayer lost.";
+  } else if (playerSelection === 'skissors' && computerSelection === 'rock') {
+    return "Rock beats skissors.\nPlayer lost.";
+  } else if (playerSelection === 'skissors' && computerSelection === 'paper') {
+    return "Skissors beat paper.\nComputer lost.";
+  } else if (playerSelection === 'rock' && computerSelection === 'rock') {
+    return "It's a tie!";
+  } else if (playerSelection === 'paper' && computerSelection === 'paper') {
+    return "It's a tie!";
+  } else if (playerSelection === 'skissors' && computerSelection === 'skissors') {
+    return "It's a tie!";
+  }
 }
