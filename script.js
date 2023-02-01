4b3bce3153f1c395452c79899868f48fc99f606c
nanachichi <116679354+nanachichi@users.noreply.github.com>
2023-02-01 15:59:28 +0200

    Remove pseudocode and refactor game() a bit

--- script.js
+++ script.js
@@ -1,27 +1,10 @@
-// Steps
-// 1. Function getComputerChoice() that
-// randomly returns 'Rock', 'Paper' or 'Scissors'.
-// 2. Function to play one round. It should take two parameters
-// function playRound(playerSelection, computerSelection)
-// and then return a string that declares the winner.
-// 3. Function game() that plays a 5 round game (call
-// the previous function 5 times).
-
-// array that consists of 'rock', 'paper' and 'scissors';
-// PROGRAM getComputerChoice()
-//   IF first item in array then return 'Rock'
-
 const rps = ['rock', 'paper', 'scissors'];
+
 function getComputerChoice(choices) {
   let choice = Math.floor(Math.random() * 3);
   return choices[choice];
 }
 
-// PROGRAM playRound(playerSelection, computerSelection)
-//   ASK user to choose 'rock', 'paper' or 'scissors'
-//   STORE the choice in a variable
-//   IF user choice is 'rock' and computer choice is 'paper'
-//     user lose
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === 'rock' && computerSelection === 'paper') {
@@ -43,13 +26,6 @@ function playRound(playerSelection, computerSelection) {
   } 
 }
 
-// PROGRAM game()
-//   LOOP playRound() 5 times
-//   IF win then add 1 to result
-//   ELSE IF lose then add subtract 1 from result
-//   IF result is more than 0
-//     PRINT 'You won the game!'
-//   ELSE print 'You lost the game!'
 
 function game() {
   let result = 0;
@@ -63,7 +39,11 @@ function game() {
     } else {
       let computerSelection = getComputerChoice(rps);
       let roundResult = playRound(playerSelection, computerSelection);
-      roundResult.includes("You") ? result -= 1 : (roundResult.includes("Computer") ? result += 1 : '');
+      if (roundResult.includes("You lost")) {
+        result -= 1;
+      } else if (roundResult.includes("Computer lost")) {
+        result += 1;
+      }
       console.log(roundResult);
     }
   }
